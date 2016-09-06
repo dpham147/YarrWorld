b52b0154235f414ef3c3e5d65c274c1203dd8384
dpham147 <dannypham92@hotmail.com>
2016-09-06 16:02:16 -0700

    must save

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/yarrworld/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.orangecoastcollege.cs273.dpham147.yarrworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button speakButton;
+    private Button speakButton;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Hook up the speakButton to the one in our view
         speakButton = (Button) findViewById(R.id.speakButton);
+
+        // Wire up the speakButton to perform an onClick event
+        speakButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Use the static reference to "Toast" to display the message
+                        // Toast vanishes after a set amount of time
+                        // Dialog requires user input
+                        Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
 
