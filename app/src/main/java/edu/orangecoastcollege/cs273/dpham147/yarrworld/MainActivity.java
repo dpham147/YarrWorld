e420d3a1df2e03297f5717ab3d59350ce94caf50
dpham147 <dannypham92@hotmail.com>
2016-09-06 15:47:47 -0700

    must save

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/yarrworld/MainActivity.java
@@ -2,14 +2,20 @@ package edu.orangecoastcollege.cs273.dpham147.yarrworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button speakButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Restoring user settings from previous session
         super.onCreate(savedInstanceState);
+        // Inflating layout activity_main.xml
         setContentView(R.layout.activity_main);
     }
 
-
+    speakButton = (Button) findViewById(R.id.speakButton);
 }
